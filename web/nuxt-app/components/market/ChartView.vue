5a82dac765c5ea2dd4ddaca6cbf3a3baacab2e13
Michael Kritikos Frager <mfrager@gmail.com>
2022-12-08 16:28:43 -0800

    Add view selection to chart

--- web/nuxt-app/components/market/ChartView.vue
+++ web/nuxt-app/components/market/ChartView.vue
@@ -1,6 +1,7 @@
 <template>
     <v-card>
         <template v-if="showChart">
+            <v-select v-model="chartView" :items="chartViewList" item-value="id" item-text="name" label="Candlesticks"/>
             <vue-apex-charts :options="chartOptions" :series="chartData" ref="apexChart"></vue-apex-charts>
         </template>
     </v-card>
@@ -26,7 +27,7 @@ export default {
         const showChart = ref(false)
         const chartRef = ref(null)
 
-        const chartOptions = {
+        const chartOptions = ref({
             chart: {
                 type: 'candlestick',
                 width: '100vw',
@@ -43,13 +44,49 @@ export default {
                     enabled: true,
                 },
             },
-        }
+        })
 
-        var chartData = ref([ { data: [] } ])
-        var loadData = async (vr, cd, sc, m) => {
-            var url = 'https://aqua-dev1.atellix.net:8000/v1/history'
-            console.log('Request history: ' + m)
-            var res = await axios.post(url, JSON.stringify({ 'market': m }), { 'Content-Type': 'application/json' })
+        const chartData = ref([ { data: [] } ])
+        const chartView = ref('v1m')
+        const chartViewList = ref([
+            {'id': 'v1m', 'name': '1 Min'},
+            {'id': 'v3m', 'name': '3 Min'},
+            {'id': 'v5m', 'name': '5 Min'},
+            {'id': 'v15m', 'name': '15 Min'},
+            {'id': 'v30m', 'name': '30 Min'},
+            {'id': 'v1h', 'name': '1 Hr'},
+            {'id': 'v2h', 'name': '2 Hr'},
+            {'id': 'v4h', 'name': '4 Hr'},
+            {'id': 'v6h', 'name': '6 Hr'},
+            {'id': 'v8h', 'name': '8 Hr'},
+            {'id': 'v12h', 'name': '12 Hr'},
+            {'id': 'v1d', 'name': '1 Day'},
+            {'id': 'v7d', 'name': '7 Day'},
+            {'id': 'v30d', 'name': '30 Day'},
+        ])
+        const chartViewRange = {
+            'v1m': (1000 * 60 * 60),
+            'v3m': (1000 * 60 * 60 * 3),
+            'v5m': (1000 * 60 * 60 * 6),
+            'v15m': (1000 * 60 * 60 * 8),
+            'v30m': (1000 * 60 * 60 * 12),
+            'v1h': (1000 * 60 * 60 * 16),
+            'v2h': (1000 * 60 * 60 * 24),
+            'v4h': (1000 * 60 * 60 * 24),
+            'v6h': (1000 * 60 * 60 * 48),
+            'v8h': (1000 * 60 * 60 * 72),
+            'v12h': (1000 * 60 * 60 * 72),
+            'v1d': (1000 * 60 * 60 * 24 * 30),
+            'v7d': (1000 * 60 * 60 * 24 * 30 * 6),
+            'v30d': (1000 * 60 * 60 * 24 * 30 * 12),
+        }
+        const loadData = async (vr, cd, sc, m, v) => {
+            console.log('Loading chart data: ' + m + ' ' + v)
+            var url = 'https://aqua-dev1.atellix.net:8000/v1/history' // TODO: Make this a config var
+            var res = await axios.post(url, JSON.stringify({
+                'market': m,
+                'view': v,
+            }), { 'Content-Type': 'application/json' })
             if (res.status === 200 && res.data.result === 'ok') {
                 var list = []
                 for (var i = 0; i < res.data.history.length; i++) {
@@ -67,6 +104,11 @@ export default {
                 }
                 list.reverse()
                 cd.value[0].data = list
+                vr.apexChart.updateOptions({
+                    xaxis: {
+                        range: chartViewRange[v],
+                    }
+                })
                 vr.apexChart.updateSeries(cd.value)
             }
         }
@@ -78,22 +120,35 @@ export default {
             }, 10)
         })
 
+        var ready = false
+        var prcScale
+        var marketAddr
         watch([market], async (current, prev) => {
             if (current[0].marketReady) {
-                var prcScale = current[0].prcTokenScale
-                await loadData(viewRefs, chartData, prcScale, current[0].marketAddr)
+                ready = true
+                prcScale = current[0].prcTokenScale
+                marketAddr = current[0].marketAddr
+                await loadData(viewRefs, chartData, prcScale, marketAddr, chartView.value)
                 setInterval(async () => {
-                    console.log('Reloading chart data: ' + current[0].marketAddr)
-                    await loadData(viewRefs, chartData, prcScale, current[0].marketAddr)
+                    await loadData(viewRefs, chartData, prcScale, marketAddr, chartView.value)
                 }, 60000)
+            }
+        })
+
+        watch([chartView], async (current, prev) => {
+            if (ready) {
+                console.log('Update chart view: ' + current[0])
+                console.log(market)
+                await loadData(viewRefs, chartData, prcScale, marketAddr, current[0])
             }
         })
 
         return {
             showChart,
+            chartView,
+            chartViewList,
             chartOptions,
             chartData,
-
             icons: {
                 mdiDotsVertical,
                 mdiTrendingUp,
---
+++ web/nuxt-app/pages/solana.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-row>
+        <v-col cols="12" md="12">
+            Solana Links
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+
+export default {
+    components: {
+        MarketList,
+    },
+    layout: "Content",
+    head() {
+        return {
+            title: 'Solana Link'
+        }
+    },
+    setup() {
+        return {}
+    },
+};
+</script>
